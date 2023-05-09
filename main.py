eb5faba2b3740eb5c5e664d19b23f03424fdea5f
jmtek <jing226@gmail.com>
2023-05-09 23:39:01 +0800

    make it works

--- main.py
+++ main.py
@@ -1,27 +1,56 @@
 import os
+import uuid
 from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 import subprocess
-from gradio_client import Client
+# from gradio_client import Client
 
-client = Client("abidlabs/music-separation")
+# client = Client("abidlabs/music-separation")
 
 def acapellify(audio_path):
-    result = client.predict(audio_path, api_name="/predict")
-    return result
+    # result = client.predict(audio_path, api_name="/predict")
+    audio_dir = os.path.dirname(audio_path)
+    audio_filename = os.path.basename(audio_path).split(".")
+    if audio_filename[1] != "wav":
+        old_audio_path = audio_path
+        audio_path = audio_dir + '/' + audio_filename[0] + '.wav'
+        subprocess.run(['ffmpeg',
+                        '-i',
+                        old_audio_path,
+                        audio_path])
+        
+    subprocess.run(['python3', 
+                    '-m', 
+                    'demucs.separate', 
+                    '-n', 
+                    'htdemucs', 
+                    '--two-stems=vocals', 
+                    '-d', 
+                    'cpu', 
+                    audio_path, 
+                    '-o', 
+                    'static'])
+    
+    output_path = f'static/htdemucs/{audio_filename[0]}/'
+
+    # os.system(f"python3 -m demucs.separate -n htdemucs --two-stems=vocals -d cpu {audio_path} -o out")
+    return f"{output_path}vocals.wav",f"{output_path}no_vocals.wav"
 
 def process_video(video_path):
-    old_audio = os.path.basename(video_path).split(".")[0] + ".m4a"
+    video_dir = os.path.dirname(video_path)
+    video_filename = os.path.basename(video_path)
+    old_audio = video_dir + '/' + video_filename.split(".")[0] + ".m4a"
     subprocess.run(['ffmpeg', '-y', '-i', video_path, '-vn', '-acodec', 'copy', old_audio])
 
     new_audio = acapellify(old_audio)
+    new_audio_path = os.path.abspath(new_audio[0])
 
-    new_video = f"acap_{video_path}"
-    #new_video = os.path.basename(video_path).split(".")[0] + "_new.mp4"
-    subprocess.call(['ffmpeg', '-y', '-i', video_path, '-i', new_audio[0], '-map', '0:v', '-map', '1:a', 
+    new_video = f'{os.path.dirname(video_path)}/{"_new.".join(video_filename.split("."))}'
+
+    subprocess.call(['ffmpeg', '-y', '-i', video_path, '-i', new_audio_path, '-map', '0:v', '-map', '1:a', 
                      '-c:v', 'copy', '-c:a', 'aac', '-strict', 'experimental', f"{new_video}"])
     return new_video
 
@@ -42,7 +71,7 @@ async def home(request: Request):
 @app.post("/uploadvideo/")
 async def upload_video(video: UploadFile = File(...)):
     # 生成文件名
-    filename = video.filename
+    filename = str(uuid.uuid1()) + '.' + video.filename.split(".")[1]
     
     # 拼接保存路径 
     save_path = os.path.join('static', filename) 
@@ -62,7 +91,7 @@ async def upload_video(video: UploadFile = File(...)):
 @app.post("/uploadaudio/")
 async def upload_audio(audio: UploadFile = File(...)):
     # 生成文件名
-    filename = audio.filename
+    filename = str(uuid.uuid1()) + '.' + audio.filename.split(".")[1]
     
     # 拼接保存路径 
     save_path = os.path.join('static', filename) 
